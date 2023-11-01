7949ea252fd450c87fb78573ec14e82d528d230b
Knyaz <qnyazface@mail.ru>
2023-11-01 14:28:15 +0400

    admin dashboard add. Areas folder structure add

---
+++ Electro Ecommerce MVC Project/Electro Ecommerce MVC Project/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Electro_Ecommerce_MVC_Project.Areas.Admin.Controllers;
+
+[Route("admin/dash")]
+[Area("admin")]
+public class DashBoardController : Controller
+{
+    [HttpGet("index")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
